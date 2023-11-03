4bafef2990230759e42691a603fc5d4844b8e258
Lieatis Sakwya <celiasash@foxmail.com>
2023-11-03 09:42:12 +0800

    哈哈2

---
+++ core/myplt.py
@@ -0,0 +1,1 @@
+def plt(**dic):
--- core/numpytest.py
+++ core/numpytest.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 from scipy import stats
 import seaborn as sns
 
@@ -15,21 +16,45 @@ L = df['厅'].to_numpy()
 area = df['房间大小'].to_numpy()
 windows = df['窗户朝向'].to_numpy()
 fixture = df['装修造型'].to_numpy()
-
 year = df['修建年份'].to_numpy()
 hType = df['房源'].to_numpy()
 price = df['平米房价'].to_numpy()
 
-districts = np.unique(district)
+
 da = []
 db = []
-for district_name in districts:
-    bool = [district == district_name]
-    da.append(price[tuple(bool)])
+dc = []
+districts = np.unique(district)
+# for district_name in districts:
+#     bool = [district == district_name]
+#     price_ = price[tuple(bool)]
+#     da.append(price_)
+#     dc.append(pd.DataFrame(price_, columns=[district_name]))
+#
+# for df in da:
+#     db.append(df[:2217])
+# da = np.asarray(db)
+# df = pd.DataFrame(da.T, columns=districts)
+# df.plot.hist(bins=100, alpha=0.5, figsize=(10, 6))
+# plt.ioff()
+# plt.show()
+# fig = plt.figure(figsize=(10, 6))
+# gs = gridspec.GridSpec(3, 5)
+# axs = []
+# for i in range(3):
+#     for j in range(5):
+#         axs.append(plt.subplot(gs[i, j]))
+# for i in range(len(dc)):
+#     df = dc[i]
+#     df.plot.hist(bins=100, alpha=0.5, figsize=(10, 6), ax=axs[i])
+#
+# plt.show()
 
-for df in da:
-    db.append(df[:2217])
-da = np.asarray(db)
-df = pd.DataFrame(da.T, columns=districts)
-df.plot.hist(bins=100, alpha=0.5, figsize=(10, 6))
+dd = np.asarray([district, fixture, hType, price]).T
+df = pd.DataFrame(dd, columns=["区域", "装修造型", "房源", "平米房价"])
+sns.stripplot(x="区域", y="平米房价", hue="房源", data=df, jitter=True, size=1)
+plt.tight_layout()
+plt.show()
+sns.violinplot(x="区域", y="平米房价", hue="房源",split=False, data=df,linewidth=0)
+plt.tight_layout()
 plt.show()
